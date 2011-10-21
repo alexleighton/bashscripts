c10897205f5264bbf95b694e211613c46f1621bb
Alex Leighton <axlelonghorn@gmail.com>
2011-10-20 20:49:49 -0700

    Add a script for use on new machines.

---
+++ init.sh
@@ -0,0 +1,10 @@
+
+############################################################
+# GIT INITIALIZATION
+
+git config --global color.ui auto
+git config --global user.name "Alex Leighton"
+git config --global github.user "alexleighton"
+git config --global user.email "axlelonghorn@gmail.com"
+
+
